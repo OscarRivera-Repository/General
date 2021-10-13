91c555900c35739e7c0fdb03fbd9f2565009b9df
Oscar Rivera <oscar.rivesa@hotmail.com>
2021-10-12 18:03:02 -0600

    se validan etiquetas

--- TRANSFOR/src/transfor/Validaciones.java
+++ TRANSFOR/src/transfor/Validaciones.java
@@ -1,6 +1,20 @@
 package transfor;
 
 public class Validaciones {
+    /*
+        Algunas validaciones ignoran los espacios vacios,
+        esta función los elimina para que no interfieran    
+    */
+    public static String QuitarEspacios(String linea){
+        String nuevaLinea = "";
+        for(int i = 0; i < linea.length(); i++){
+            if(linea.charAt(i) != ' ' ){ 
+                nuevaLinea += linea.charAt(i); 
+            }
+        }
+        return nuevaLinea;
+    }
+    
     public static String Solo80Caracteres(String linea){
         if(linea.length() > 80){
             return "Error 001: Línea mayor a 80 caracteres";
@@ -10,13 +24,21 @@ public class Validaciones {
     
     public static String EtiquetasCorrectas(String linea){
         String error = "Error 002: Etiqueta inválida";
-        if (linea.length() < 5){return error;}
-        String inicio = linea.substring(0, 4);
+        // La etiqueta es de tamaño 5 seguida de un espacio
+        if (linea.length() < 6){return error;}
+        String inicio = linea.substring(0, 5);
+        // La equeta debe ser un numero y ocupar 5 caracteres
         try {
             double d = Integer.parseInt(inicio);
         } catch (NumberFormatException nfe) {
+            // Si los primeros 5 caracteres no son número es un  error
             return error;
         }
+        // Depués del número debe haber espacio o es un error
+        char posicion6 = linea.charAt(5);
+        
+        if(posicion6 != ' '){return error;}
+
         return "";
     }
    
